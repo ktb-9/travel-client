bc9e1b4390f726dc0b320951c3bacdb4ea1c6b63
kwangyoulsagong <sgky0511@naver.com>
2024-10-14 17:42:41 +0900

    feat: 다시 복귀

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,41 @@
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
-import { RecoilRoot } from "recoil";
-interface RootLayoutProps {
-  children: ReactNode; // children의 타입을 명시적으로 설정
-}
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
+
+import { useColorScheme } from "@/hooks/useColorScheme";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
-    <RecoilRoot>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
       </Stack>
-    </RecoilRoot>
+    </ThemeProvider>
   );
 }
