a165cdab4e82d4a4b072a050edaf4f53cb0c11af
kwangyoulsagong <sgky0511@naver.com>
2024-10-15 15:22:40 +0900

    feat: 핫플레이스 ui 디자인 완료

--- components/common/hotplace/hotplace.tsx
+++ components/common/hotplace/hotplace.tsx
@@ -1,26 +1,38 @@
-import { View, Image } from "react-native";
+import { View, Image, Text } from "react-native";
 import styles from "./styles";
 import jeju from "@/assets/images/jeju.png";
 import busan from "@/assets/images/busan.png";
-type Destination = {
-  destination: any; // You might want to use a more specific type for the image
+import heart from "@/assets/images/iconheart.png";
+type dataState = {
+  destination: any;
+  mainDescription: string;
+  subDescription: string;
 };
 const HotPlace = () => {
-  const data: Destination[] = [
+  const data: dataState[] = [
     {
       destination: jeju,
+      mainDescription: "설렘으로 가득한 노을",
+      subDescription: "제주에서 추억 만들러 갈까요?",
     },
     {
       destination: busan,
+      mainDescription: "스트레스 풀 땐 바다지",
+      subDescription: "부산함 갈까!",
     },
   ];
 
   return (
     <View style={styles.container}>
-      {data.map((value: Destination, index: number) => (
-        <View key={index} style={styles.box}>
+      {data.map((value: dataState) => (
+        <View style={styles.box}>
           <Image source={value.destination} style={styles.image} />
           <View style={styles.overlay} />
+          <View style={styles.content}>
+            <Text style={styles.mainTitle}>{value.mainDescription}</Text>
+            <Text style={styles.subTitle}>{value.subDescription}</Text>
+            <Image source={heart} style={styles.icon} />
+          </View>
         </View>
       ))}
     </View>
--- components/common/hotplace/styles.tsx
+++ components/common/hotplace/styles.tsx
@@ -22,13 +22,40 @@ const styles = StyleSheet.create({
     right: 0,
     bottom: 0,
 
-    backgroundColor: "rgba(76, 74, 74, 0.45)",
+    backgroundColor: "rgba(76, 74, 74, 0.35)",
     borderRadius: 25,
   },
   image: {
     width: "100%",
     height: "100%",
     borderRadius: 25,
+  },
+  content: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    borderColor: "black",
+    alignItems: "center", // 추가
+    justifyContent: "center",
+    zIndex: 999999,
+    gap: 4,
+  },
+  mainTitle: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  subTitle: {
+    color: "#E5E5E5",
+    textAlign: "center",
+    fontSize: 10,
+    fontWeight: "bold",
+  },
+  icon: {
+    width: 25,
+    height: 25,
   },
 });
 export default styles;
