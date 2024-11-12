ee32b317d9dbd6f3435b9ae01144fe409297c793
kwangyoulsagong <sgky0511@naver.com>
2024-11-12 11:23:54 +0900

    feat: 네이티브 앱에서 데이터 전달 받기 기능

--- app/image/image.tsx
+++ app/image/image.tsx
@@ -1,13 +1,28 @@
 import { View } from "react-native";
 import WebView from "react-native-webview";
 import Header from "./header/header";
+import { useEffect, useRef } from "react";
 
 const ImageView = () => {
+  const webViewRef = useRef<WebView>(null);
+  // 웹뷰가 로드되면
+  const onLoad = () => {
+    const groupId = 1;
+    const postId = 1;
+    // 데이터를 직렬화 해서 전송
+    if (webViewRef.current) {
+      webViewRef.current.postMessage(JSON.stringify({ groupId, postId }));
+    }
+  };
   return (
     <View style={{ flex: 1 }}>
       <Header />
-
-      <WebView source={{ uri: "http://localhost:3000/" }} style={{ flex: 1 }} />
+      <WebView
+        ref={webViewRef as any}
+        source={{ uri: "http://localhost:3000/" }}
+        style={{ flex: 1 }}
+        onLoad={onLoad}
+      />
     </View>
   );
 };
